d2c7814f5639dc966bd653aa391c34380eb5870d
Lesley Paone <lesley@dh42.com>
2019-02-24 11:51:02 -0600

    Removed relative urls

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -1,37 +1,37 @@
 
 var documents = [{
     "id": 0,
-    "url": "http://localhost:4000/404.html",
+    "url": "/404.html",
     "title": "404",
     "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "http://localhost:4000/about",
+    "url": "/about",
     "title": "Mediumish Template for Jekyll",
     "body": "  This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Features: Built for JekyllCompatible with Github pagesFeatured PostsIndex PaginationSEOFeedSitemapPost SharePost CategoriesPrev/Next LinkCategory Archives (Compatible with Github pages)Jumbotron CategoriesIntegrations:Disqus CommentsGoogle AnalayticsMailchimp IntegrationDesign Features:Bootstrap v4. xFont AwesomeMasonryLayouts:DefaultPostPageArchiveCategories (for 100% compatibility with Github pages)What's Jekyll: If you aren't familiar with Jekyll yet, you should know that it is a static site generator. It will transform your plain text into static websites and blogs. No more databases, slow loading websites, risk of being hacked. . . just your content. And not only that, with Jekyll you get free hosting with GitHub Pages! This page itself is free hosted on Github with the help of Jekyll and Mediumish template that you're currently previewing. If you are a beginner we recommend you start with Jekyll's Docs. Now if you know how to use Jekyll, let's move on to using Mediumish template in Jekyll: How to use  Mediumish  theme: Download or git clone https://github. com/wowthemesnet/mediumish-theme-jekyll. gitcd mediumish-theme-jekyllbundleEdit _config. yml options. If your site is in root: baseurl: ''. Also, change your Google Analytics code, disqus username, authors, Mailchimp list etc. jekyll serve --watchStart by adding your . md files in _posts. Mediumish already has a few examples. YAML front matterfeatured post - featured:trueexclude featured post from  All stories  loop to avoid duplicated posts - hidden:truepost image - image: assets/images/mypic. jpgexternal post image - image:  https://externalwebsite. com/image4. jpg page comments - comments:truemeta description (optional) - description:  this is my meta description YAML Post Example:---layout: posttitle:  We all wait for summer author: johncategories: [ Jekyll, tutorial ]image: assets/images/5. jpgfeatured: true---YAML Page Example---layout: pagetitle: Mediumish Template for Jekyllcomments: true---Questions or bug reports?: Head over to our Github repository!  Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish .    $5. 00  $10. 00  $25. 00  $50. 00  $100. 00"
     }, {
     "id": 2,
-    "url": "http://localhost:4000/categories",
+    "url": "/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 3,
-    "url": "http://localhost:4000/contact",
+    "url": "/contact",
     "title": "Get in contact with me",
     "body": ""
     }, {
     "id": 4,
-    "url": "http://localhost:4000/",
+    "url": "/",
     "title": "Home",
     "body": "      Featured:                                                                                                                                                                                 Starting Growing:               If you are familiar with me, you know that I work from an office in my home. I have an awesome commute, but it gets. . . :                                                                                                                                       Lesley Paone                                20 Feb 2018                                                                                                                                All Stories:                         "
     }, {
     "id": 5,
-    "url": "http://localhost:4000/robots.txt",
+    "url": "/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 6,
-    "url": "http://localhost:4000/growing-things/",
+    "url": "/growing-things/",
     "title": "Starting Growing",
     "body": "2018/02/20 - If you are familiar with me, you know that I work from an office in my home. I have an awesome commute, but it gets boring looking at the same walls every day. So lately I have started picking up more hobbies. The newest hobby is trying my hand at growing things. I bought some succulents at first, which I have managed to keep alive for several months.  "
     }];
